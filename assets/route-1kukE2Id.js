9d37b2104278999160fb25bb53de0d9d910dba7b
Chris <chris@ditto.com>
2026-09-22 17:25:13 +0800

    build: update generated html

--- assets/route-1kukE2Id.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./authenticated-layout-Cx1j8oDx.js";var t=e;export{t as component};
---
+++ assets/route-Chenug3o.js
@@ -0,0 +1,1 @@
+import{t as e}from"./authenticated-layout-BbVqyDzm.js";var t=e;export{t as component};
